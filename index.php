dd41a5bc0bee068d89611cc4827eb0d9479dc616
Sourov Pal <85163889+sourovpal@users.noreply.github.com>
2022-09-05 11:50:55 +0600

    Create index.php

---
+++ index.php
@@ -0,0 +1,6 @@
+php -f /home/mediexam/medicalwallah.com/admin/time-command-shiddle.php > /dev/null 2>&1
+
+/path/to/bin/php /path/to/script.php; sleep 10; /path/to/bin/php /path/to/script.php; sleep 10; /path/to/bin/php /path/to/script.php; sleep 10;
+/path/to/bin/php /path/to/script.php; sleep 10; /path/to/bin/php /path/to/script.php; sleep 10; /path/to/bin/php /path/to/script.php
+
+-*/5-22 * * * sleep 5;your_script.php
