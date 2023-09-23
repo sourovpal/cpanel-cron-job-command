46cf5a3d9816ca2751857c0b85bf10fdcc3d702d
Sourov Pal <85163889+sourovpal@users.noreply.github.com>
2023-09-23 11:26:03 +0600

    Update index.php

--- index.php
+++ index.php
@@ -4,3 +4,7 @@ php -f /home/mediexam/medicalwallah.com/admin/time-command-shiddle.php > /dev/nu
 /path/to/bin/php /path/to/script.php; sleep 10; /path/to/bin/php /path/to/script.php; sleep 10; /path/to/bin/php /path/to/script.php
 
 -*/5-22 * * * sleep 5;your_script.php
+
+
+
+while : ; do sleep 1 ; /usr/local/bin/php /home/ptl4606q9941/public_html/dev.pvtoev.com.au/index.php || break ; done
